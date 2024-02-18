0c8e8e5d962b51c73d3cc1984969b1c63b836b13
insuwathaiwan <insuwathaiwan@central.co.th>
2024-02-18 17:24:20 +0700

    Implement API security with Basic authen and API key

---
+++ posttest/src/main/java/com/kbtg/bootcamp/posttest/Security/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.kbtg.bootcamp.posttest.Security;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.stereotype.Component;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Component
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+
+        return http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((requests) -> requests
+                        .anyRequest().authenticated())
+                .addFilterBefore(new ApiKeyAuthFilter(), BasicAuthenticationFilter.class)
+                .httpBasic(withDefaults())
+                .build();
+    }
+}
